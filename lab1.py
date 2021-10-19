bd7577a72ee38d89cb7614a95a2ed6dfa1157c5a
kryzin <karolina.ryzinska@student.uwm.edu.pl>
2021-10-19 11:00:49 +0200

    zadania do 8

---
+++ lab1.py
@@ -0,0 +1,83 @@
+# zad1
+
+
+# def zad1(i: str, n: str) -> str:
+#     nazwa = i[0] + "." + n
+#     return nazwa
+#
+#
+# print(zad1("Jan", "Kowalski"))
+
+# zad2
+
+
+# def zad2(i: str, n: str) -> str:
+#     nazwa = i[0].capitalize() + "." + n.capitalize()
+#     return nazwa
+#
+#
+# print(zad2("jan", "kowalski"))
+
+# zad3
+
+
+# def zad3(rok1: int, rok2: int, wiek: int) -> int:
+#     return rok1*100 + rok2 - wiek
+#
+#
+# print(zad3(20, 21, 20))
+
+# zad4
+
+
+# def zad4(i: str, n: str, zad2):
+#     return zad2(i, n)
+#
+#
+# print(zad4("jan", "Kowalski"))
+
+
+# zad5
+
+
+# def zad5(a: int, b: int) -> float:
+#     if a > 0 and b >= 0:
+#         return a / b
+#
+#
+# print(zad5(5, 2))
+
+# zad6
+
+# suma = 0
+# while suma < 100:
+#     suma += int(input("podaj kolejną liczbę"))
+#     print("suma: ", suma, "do 100 brakuje: ", 100-suma)
+#
+# print(suma)
+
+
+# zad7
+
+
+# def zad7(lista):
+#     krotka = tuple(lista)
+#     return krotka
+#
+#
+# print(zad7(lista = ["lody", 3, "bajka"]))
+
+
+# zad8
+
+# pom = 0
+# lista8 = []
+# while pom < 5:
+#     lista8.append(input("podaj element listy"))
+#     pom += 1
+#
+#
+# krotka8 = tuple(lista8)
+# print(krotka8)
+
+
