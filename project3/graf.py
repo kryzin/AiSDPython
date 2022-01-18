393da8bd40782be7f099e663c5ad96f5db3bebce
kryzin <karolina.ryzinska@student.uwm.edu.pl>
2022-01-18 18:55:59 +0100

    pr3

---
+++ project3/graf.py
@@ -0,0 +1,146 @@
+from project1 import Queue
+
+class Edge:
+    def __init__(self, data):
+        self.vertex = data
+        self.next = None
+
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [None] * self.V
+
+    # Function to add an edge in an undirected graph
+    def add_edge(self, src, dest):
+        # Adding the node to the source node
+        node = Edge(dest)
+        node.next = self.graph[src]
+        self.graph[src] = node
+
+        # Adding the source node to the destination as
+        # it is the undirected graph
+        node = Edge(src)
+        node.next = self.graph[dest]
+        self.graph[dest] = node
+
+    # Function to print the graph
+    def print_graph(self):
+        for i in range(self.V):
+            print("Adjacency list of vertex {}\n head".format(i), end="")
+            temp = self.graph[i]
+            while temp:
+                print(" -> {}".format(temp.vertex), end="")
+                temp = temp.next
+            print(" \n")
+
+    def BFS(self, s):
+
+        # Mark all the vertices as not visited
+        visited = [False] * (max(self.graph) + 1)
+
+        # Create a queue for BFS
+        queue = []
+
+        # Mark the source node as
+        # visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+
+            # Dequeue a vertex from
+            # queue and print it
+            s = queue.pop(0)
+            print(s, end=" ")
+
+            # Get all adjacent vertices of the
+            # dequeued vertex s. If a adjacent
+            # has not been visited, then mark it
+            # visited and enqueue it
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+    def DFSUtil(self, v, visited):
+
+        # Mark the current node as visited
+        # and print it
+        visited.add(v)
+        print(v, end=' ')
+
+        # Recur for all the vertices
+        # adjacent to this vertex
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.DFSUtil(neighbour, visited)
+
+    # The function to do DFS traversal. It uses
+    # recursive DFSUtil()
+    def DFS(self, v):
+
+        # Create a set to store visited vertices
+        visited = set()
+
+        # Call the recursive helper function
+        # to print DFS traversal
+        self.DFSUtil(v, visited)
+
+    def minDistance(self, dist, sptSet):
+
+        # Initialize minimum distance for next node
+        min = sys.maxsize
+
+        # Search not nearest vertex not in the
+        # shortest path tree
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+
+        return min_index
+
+    def printSolution(self, dist):
+        print("Vertex tDistance from Source")
+        for node in range(self.V):
+            print(node, "t", dist[node])
+
+    def dijkstra(self, v):
+
+        dist = [sys.maxsize] * self.V
+        dist[v] = 0
+        sptSet = [False] * self.V
+
+        for cout in range(self.V):
+
+            # Pick the minimum distance vertex from
+            # the set of vertices not yet processed.
+            # u is always equal to src in first iteration
+            u = self.minDistance(dist, sptSet)
+
+            # Put the minimum distance vertex in the
+            # shortest path tree
+            sptSet[u] = True
+
+            # Update dist value of the adjacent vertices
+            # of the picked vertex only if the current
+            # distance is greater than new distance and
+            # the vertex in not in the shortest path tree
+            for vert in range(self.V):
+                if self.graph[u][vert] > 0 and sptSet[vert] == False and dist[vert] > dist[u] + self.graph[u][vert]:
+                    dist[vert] = dist[u] + self.graph[u][vert]
+
+
+# Driver program to the above graph class
+V = 5
+graph = Graph(V)
+graph.add_edge(0, 1)
+graph.add_edge(0, 4)
+graph.add_edge(1, 2)
+graph.add_edge(1, 3)
+graph.add_edge(1, 4)
+graph.add_edge(2, 3)
+graph.add_edge(3, 4)
+
+graph.print_graph()
+graph.dijkstra(0)
